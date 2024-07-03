009f06cc7feebf8fe90fe1c6c1e06f3b333bd55e
CIBA <sanchezmariabelen01@gmail.com>
2024-07-03 17:08:38 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="totalContainer">
+    <div class="icon">
+      <img src="../assets/images/iconoPage.png" alt="logo" />
+    </div>
+    <div class="license">
+      <h4>Copyright © 2024 infoCatEmbalses</h4>
+    </div>
+    <div class="info">
+      <h4>Legal information / Privacy policy</h4>
+    </div>
+    <div class="socialMedia"><</div>
+    <img src="../assets/images/instagram.png" alt="instagram" />
+    <img src="../assets/images/whatsapp" alt="whatsapp" />
+    <img src="../assets/images/share" alt="share" />
+  </div>
+</template>
+
+<style scoped>
+.totalContainer {
+}
+</style>
