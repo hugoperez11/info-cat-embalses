25dc28a75fdf99fc355fdabe143f0346c044d82d
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-07-04 11:07:35 +0200

    table structure and imports

--- src/views/AboutUsView.vue
+++ src/views/AboutUsView.vue
@@ -2,16 +2,4 @@
   <h1>About Us</h1>
 </template>
 
-<style>
-<<<<<<< HEAD
-/* @media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-} */
-=======
-
->>>>>>> d18d4e4889fc62d88268e59e90656b39241b254e
-</style>
+<style></style>
