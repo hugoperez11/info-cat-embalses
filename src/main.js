bc1c9808bd4723b0a870e2233d2b78a742813b20
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-07-04 22:14:09 +0200

    uncomment main.js imports

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-// import PrimeVue from "primevue/config";
-// import DataTable from "primevue/datatable";
-// import Column from "primevue/column";
+import PrimeVue from "primevue/config";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
 // import "primevue/resources/themes/saga-blue/theme.css";
 // import "primevue/resources/primevue.min.css";
-// import "primeicons/primeicons.css";
-// import "primeflex/primeflex.css";
+import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 import { createPinia } from "pinia";
 import router from "./router";
 
