43360cf399aa491f24fe418a1fe4b9215c1cab9b
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-07-04 20:31:20 +0200

    table finished

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,171 +1,352 @@
-<!-- <script setup></script>
-
-<template>
-  <div class="container">
-    <DataTable
-      :value="customers"
-      paginator
-      :rows="5"
-      :rowsPerPageOptions="[5, 10, 20, 50]"
-      tableStyle="min-width: 50rem"
-    >
-      <Column field="name" header="Name" style="width: 25%"></Column>
-      <Column field="country.name" header="Country" style="width: 25%"></Column>
-      <Column field="company" header="Company" style="width: 25%"></Column>
-      <Column
-        field="representative.name"
-        header="Representative"
-        style="width: 25%"
-      ></Column>
-    </DataTable>
-  </div>
-</template>
-
-<style scoped>
-.container {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 90vw;
-  width: 100%;
-  background-color: #bde3ff;
-}
-</style> -->
-
 <script setup>
 import { ref } from "vue";
 
+// Datos de ejemplo para la tabla
 const customers = ref([
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
+    dia: "04/07/24",
+    estacion: "Embalse 1",
     nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    porcentaje: "75%",
+    vol: "150",
   },
   {
-    dia: { name: "04/07/24" },
-    estacion: { name: "Embalse 1" },
-    nivelabsoluto: "150200",
-    porcentaje: { name: "75%" },
-    vol: { name: "75510" },
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
   },
-
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 1",
+    nivelabsoluto: "150200",
+    porcentaje: "75%",
+    vol: "150",
+  },
+  {
+    dia: "04/07/24",
+    estacion: "Embalse 2",
+    nivelabsoluto: "130200",
+    porcentaje: "70%",
+    vol: "140",
+  },
   // Agrega más datos según sea necesario
 ]);
 </script>
 
 <template>
-  <div class="container">
+  <div class="table-container">
     <DataTable
       :value="customers"
       paginator
-      :rows="10"
-      :rowsPerPageOptions="[5, 10, 20, 50]"
+      :rows="20"
+      :rowsPerPageOptions="[5, 10, 20]"
       tableStyle="min-width: 50rem"
     >
-      <Column field="dia.name" header="Día" style="width: 25%"></Column>
-      <Column
-        field="estacion.name"
-        header="Estación"
-        style="width: 25%"
-      ></Column>
+      <Column field="dia" header="Día" style="width: 20%"></Column>
+      <Column field="estacion" header="Estación" style="width: 20%"></Column>
       <Column
         field="nivelabsoluto"
         header="Nivel absoluto"
-        style="width: 25%"
+        style="width: 20%"
       ></Column>
       <Column
-        field="porcentaje.name"
+        field="porcentaje"
         header="Porcentaje de volumen"
-        style="width: 25%"
+        style="width: 20%"
       ></Column>
-      <Column
-        field="vol.name"
-        header="Volumen total"
-        style="width: 25%"
-      ></Column>
+      <Column field="vol" header="Volumen total" style="width: 20%"></Column>
     </DataTable>
   </div>
 </template>
 
 <style scoped>
-.container {
+.table-container {
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 90vh; /* Cambiado de vw a vh para una mejor altura relativa */
+  flex-direction: column;
+  height: calc(122vh - 150px); /* Ajusta según el tamaño del header y footer */
   width: 100%;
+  padding: 20px;
+  box-sizing: border-box;
   background-color: #bde3ff;
+  overflow: auto; /* Permitir desplazamiento si el contenido excede la altura */
+}
+
+.custom-datatable .p-datatable {
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+}
+
+.custom-datatable .p-datatable-header,
+.custom-datatable .p-datatable-footer {
+  background-color: #007ad9;
+  color: white;
+  border-radius: 10px 10px 0 0;
+}
+
+.custom-datatable .p-datatable-thead > tr > th {
+  background-color: #007ad9;
+  color: white;
+  text-align: left;
+  padding: 10px;
+}
+
+.custom-datatable .p-datatable-tbody > tr > td {
+  text-align: left;
+  padding: 12px 8px;
+  border-bottom: 1px solid #e0e0e0;
+}
+
+.custom-datatable .p-datatable-tbody > tr:nth-child(even) > td {
+  background-color: #f9f9f9;
+}
+
+.custom-datatable .p-datatable-tbody > tr:hover > td {
+  background-color: #f1f1f1;
+}
+
+.custom-datatable .p-paginator {
+  background-color: #007ad9;
+  color: white;
+  border-top: none;
+  border-radius: 0 0 10px 10px;
+}
+
+.custom-datatable .p-paginator .p-paginator-current,
+.custom-datatable .p-paginator .p-paginator-icon,
+.custom-datatable .p-paginator .p-paginator-page,
+.custom-datatable .p-paginator .p-paginator-first,
+.custom-datatable .p-paginator .p-paginator-last,
+.custom-datatable .p-paginator .p-paginator-next,
+.custom-datatable .p-paginator .p-paginator-prev {
+  color: white;
 }
 </style>
