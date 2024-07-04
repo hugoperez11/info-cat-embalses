41edbfb92a9bc288fb6cb843bd54b9960defe21e
Hugo Perez Santos <hugopsantos@hotmail.com>
2024-07-04 09:37:30 +0200

    header created

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ import { RouterLink, RouterView } from 'vue-router'
 </template>
 
 <style scoped>
-header {
+/* header {
   line-height: 1.5;
   max-height: 100vh;
 }
@@ -72,5 +72,5 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
-}
+} */
 </style>
