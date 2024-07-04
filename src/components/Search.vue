3e0ab2360464259ebeb6b171b69e6ba0f5dd83b9
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-07-04 15:43:07 +0200

    first table's view

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,4 +1,4 @@
-<script setup></script>
+<!-- <script setup></script>
 
 <template>
   <div class="container">
@@ -27,6 +27,144 @@
   justify-content: center;
   align-items: center;
   height: 90vw;
+  width: 100%;
+  background-color: #bde3ff;
+}
+</style> -->
+
+<script setup>
+import { ref } from "vue";
+
+const customers = ref([
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+  {
+    dia: { name: "04/07/24" },
+    estacion: { name: "Embalse 1" },
+    nivelabsoluto: "150200",
+    porcentaje: { name: "75%" },
+    vol: { name: "75510" },
+  },
+
+  // Agrega más datos según sea necesario
+]);
+</script>
+
+<template>
+  <div class="container">
+    <DataTable
+      :value="customers"
+      paginator
+      :rows="10"
+      :rowsPerPageOptions="[5, 10, 20, 50]"
+      tableStyle="min-width: 50rem"
+    >
+      <Column field="dia.name" header="Día" style="width: 25%"></Column>
+      <Column
+        field="estacion.name"
+        header="Estación"
+        style="width: 25%"
+      ></Column>
+      <Column
+        field="nivelabsoluto"
+        header="Nivel absoluto"
+        style="width: 25%"
+      ></Column>
+      <Column
+        field="porcentaje.name"
+        header="Porcentaje de volumen"
+        style="width: 25%"
+      ></Column>
+      <Column
+        field="vol.name"
+        header="Volumen total"
+        style="width: 25%"
+      ></Column>
+    </DataTable>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 90vh; /* Cambiado de vw a vh para una mejor altura relativa */
   width: 100%;
   background-color: #bde3ff;
 }
--- src/main.js
+++ src/main.js
@@ -1,16 +1,13 @@
-/* import './assets/main.css' */
 import { createApp } from "vue";
 import App from "./App.vue";
 import PrimeVue from "primevue/config";
 import DataTable from "primevue/datatable";
 import Column from "primevue/column";
-// import "primevue/resources/themes/saga-blue/theme.css"; // Puedes cambiar el tema
+// import "primevue/resources/themes/saga-blue/theme.css";
 // import "primevue/resources/primevue.min.css";
 import "primeicons/primeicons.css";
 import "primeflex/primeflex.css";
-
 import { createPinia } from "pinia";
-
 import router from "./router";
 
 const app = createApp(App);
@@ -19,7 +16,7 @@ app.use(createPinia());
 app.use(router);
 app.use(PrimeVue);
 
-app.mount("#app");
-
 app.component("DataTable", DataTable);
 app.component("Column", Column);
+
+app.mount("#app");
