db2aaed121c7ce4bb7c65007d488bc0c762d5246
Esther Sarasua <69312370+Fire-Fairy84@users.noreply.github.com>
2024-07-04 09:52:56 +0200

    merged changes

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,10 @@
 <script setup>
-import Header from "./components/Header.vue"
-import { RouterLink, RouterView } from 'vue-router'
-
+import Header from "./components/Header.vue";
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-
- <Header />
+  <Header />
 
   <RouterView />
 </template>
