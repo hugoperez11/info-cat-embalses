c415987545ffa9d232ae9d7c6cfaaac188b5ea1f
Conchy <xConchyx@gmail.com>
2024-07-04 16:28:00 +0200

    added about us

--- src/components/AboutUs.vue
+++ src/components/AboutUs.vue
@@ -5,21 +5,30 @@
     <img class="image" src="../assets/images/imageLogo.png" alt="logo" />
     <div class="container">
       <h3 class="h3">About us</h3>
-      <p class="p">
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni eaque
-        voluptates illo non, quaerat optio sunt assumenda cumque omnis
-        accusantium sapiente quo alias ipsum. Hic, fuga suscipit. Hic, velit
-        quisquam! Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-        Magni eaque voluptates illo non, quaerat optio sunt assumenda cumque
-        omnis accusantium sapiente quo alias ipsum. Hic, fuga suscipit. Hic,
-        velit quisquam!
-      </p>
+      <h2>Sobre Nosotros</h2>
+
+      <p>Bienvenido a <strong>InfoCatEmbalses</strong>, la aplicación líder para la gestión y monitoreo de embalses. Nuestro objetivo es
+        proporcionar información precisa y al día, sobre los niveles de agua, calidad, y estado de los embalses
+        en Cataluña. Con <strong>InfoCatEmbalses</strong>, tanto usuarios individuales como instituciones pueden tomar decisiones informadas
+        para el uso sostenible y eficiente de los recursos hídricos.</p>
+
+      <h2>Nuestra Misión</h2>
+      <p>En <strong>InfoCatEmbalses</strong>, estamos comprometidos con la preservación del medio ambiente y la gestión responsable del agua.</p>
+      <p>Nuestra misión es:</p>
+
+      <ul>
+        <li>Proveer datos precisos y actualizados sobre los embalses.</li>
+        <li>Fomentar la conciencia sobre la importancia de los recursos hídricos.</li>
+        <li>Facilitar la toma de decisiones basada en datos para gobiernos, empresas y ciudadanos.</li>
+      </ul>  
+      
     </div>
   </section>
 </template>
 
 <style scoped>
 .divContainer {
+  text-align: justify;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -27,18 +36,20 @@
   width: 100%;
   background-color: #bde3ff;
 }
+
 .container {
-  display:flex;
-  flex-direction: column; 
-  width: 600px;
+  display: flex;
+  flex-direction: column;
+  width: 700px;
 }
 
 .h3 {
   color: black;
   margin-bottom: 20px;
-  font-size:30px;
-  
+  font-size: 30px;
+
 }
+
 .p {
   color: black;
   line-height: 2.5;
